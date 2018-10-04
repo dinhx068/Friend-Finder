25f7fdc20cd56b799f6e8ea73ab058f051886ba4
Joe <dinhx068@umn.edu>
2018-10-04 15:52:42 -0500

    deleted extra lines

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -29,10 +29,7 @@ var friendsArray = [
         5, 1, 4, 4, 5, 
         1, 2, 5, 4, 1
         ]
-    }, {
-
     }
-      
 ]
 
 // Note how we export the array. This makes it accessible to other files using require.
