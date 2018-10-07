86ac9dd2f9883aef3418a5bca3993bf5fc6fd4c2
Joe <dinhx068@umn.edu>
2018-10-06 20:57:26 -0500

    done?

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,48 @@
 var friendsArray = [
     {
-        "name":"Place holder",
-        "photo":"",
+        "name":"Elmo",
+        "photo":"https://m.media-amazon.com/images/S/aplus-media/vc/6ee642df-333d-4f84-a381-4281b32b94aa._SR300,300_.png",
         "scores":[
-        5, 
-        1, 
-        4, 
-        4, 
-        5, 
-        1, 
-        2, 
-        5, 
-        4, 
-        1
+            5, 
+            1, 
+            4, 
+            4, 
+            5, 
+            1, 
+            2, 
+            5, 
+            4, 
+            1
+        ]
+    }, {
+        "name": "1",
+        "photo": "https://bciconline.com/wp-content/uploads/2017/10/1-11.png",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }, {
+        "name": "Rando",
+        "photo": "https://vignette.wikia.nocookie.net/dragonballfanon/images/7/70/Random.png/revision/latest?cb=20161221030547",
+        "scores": [
+            4,
+            2,
+            5,
+            2,
+            5,
+            1,
+            4,
+            1,
+            4,
+            2
         ]
     }
 ]
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,9 +5,41 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
-      
-    app.post("/api/home", function(req, res) {
-        friends.push(req.body);
-        res.json(true);
+
+	app.post('/api/friends', function(req, res) {
+    // Creating object to hold the best match
+    var bestMatch = {
+        name: "",
+        photo: "",
+        friendDifference: 1000
+    };
+    // Parsing user input from the form
+    var newPerson = req.body;
+    var newUserName = newPerson.name;
+    var newUserPhoto = newPerson.photo;
+    var newUserScores 	= newPerson.scores;
+    var totalDifference = 0;
+
+    // Loop through each friend in the list
+    for  (let i = 0; i < friends.length; i++) {
+        totalDifference = 0;
+        // Loop through the score array of each friend
+        for (let j = 0; j < friends[i].scores[j]; j++){
+            // Calculate the absolute value difference between the user's score and the current friend 
+            // score as a summation called totalDifference
+            totalDifference += Math.abs(parseInt(newUserScores[j]) - parseInt(friends[i].scores[j]));
+            // If the new summation of the differences is less then the differences of the current "best match"
+            if (totalDifference <= bestMatch.friendDifference) {
+                // Reset the bestMatch to be the new friend. 
+                bestMatch.name = friends[i].name;
+                bestMatch.photo = friends[i].photo;
+                bestMatch.friendDifference = totalDifference;
+            }
+        }
+    }
+    // Finally save the user's data to the database
+    friends.push(newPerson);
+    // Return a JSON with the user's bestMatch
+    res.json(bestMatch);
     });
 };
